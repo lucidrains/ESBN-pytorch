e04b6f50eb36027e461eda666e270570ab2e3594
Phil Wang <lucidrains@gmail.com>
2021-01-03 21:30:23 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'esbn-pytorch',
+  packages = find_packages(),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Emergent Symbol Binding Network - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/ESBN-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'neuro-symbolic',
+    'abstract reasoning',
+    'memory'
+  ],
+  install_requires=[
+    'torch>=1.6',
+    'einops>=0.3'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
